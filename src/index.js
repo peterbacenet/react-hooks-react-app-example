1808e8953136d6a76fc061dc853fa1bd2212e4a3
Peter Bacenet <peterbacenet@icloud.com>
2021-08-30 12:05:45 -0400

    Added TestComponent under ExampleComponent, Updated H1

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+//ReactDOM is a npm package which takes in 2 arguments, the high level React Component, in this case "App /" and WHERE we want the component to be rendered, a div with the ID of root
+//ReactDOM.render() will always be used in every application. 
